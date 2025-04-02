59a8003596b5921a62237a5134ac686c89c6c83a
Jeffery Antoniuk <jeffery.antoniuk@ualberta.ca>
2025-04-02 17:02:34 -0600

    Tweak delta idea description.

--- jupiter_output_scripts/jupiter_delta.rb
+++ jupiter_output_scripts/jupiter_delta.rb
@@ -13,11 +13,14 @@ class ChangesReport
     CSV.open(@output_file, 'wb') do |csv|
       csv << ['type', 'id', 'changed at', 'event', 'delta']
       PaperTrail::Version.where(created_at: @date..).find_each do |row|
-        # change delta for Item & Thesis type converting end result
-        # to DSpace/Scholaris key/value by extending the linked method
-        # test if the key exsists in the object_changes and creating a new
-        # structure in a new column listing the Scholaris key/value pairs to update
+        # How to communicate key/value mapping differences from Jupiter to DSpace?
+        # First part, add documentation describing how to use the output
+        # Second part, add a new column that lists the new Scholaris key plus the newly transformed value 9if applicable)
+        # Psuedocode: take the change delta for Item & Thesis type converting end result
+        # to DSpace/Scholaris key/value by extending this method and class:
         # https://gist.github.com/lagoan/839cf8ce997fa17b529d84776b91cdac#file-export_collections_csv-rb-L181-L196
+        # I.e., test if the key exists in the object_changes and creating a new
+        # structure in a new column listing the Scholaris key/value pairs to update
         csv << [row.item_type, row.item_id, row.created_at, row.event, row.object_changes]
       end
     end
